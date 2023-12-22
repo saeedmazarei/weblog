5c034dcb930264f00cb2be566bcd4d25acf0aca7
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-23 01:25:17 +0330

    Refactor loading component to add priority false to image

--- app/components/shared/Loading.tsx
+++ app/components/shared/Loading.tsx
@@ -4,7 +4,13 @@ import Image from 'next/image'
 function Loading() {
     return (
         <div className={styles['loading-container']}>
-            <Image src="/assets/images/loading.gif" alt="Loading" width={100} height={100} />
+            <Image
+                src="/assets/images/loading.gif"
+                alt="Loading"
+                width={100}
+                height={100}
+                priority={false}
+            />
         </div>
     )
 }
