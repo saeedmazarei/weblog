8b5694acc9e4b10b00e39ba2bf41f777f28fd58f
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-22 14:11:57 +0330

    Add api request to AllPosts component to fetch data

--- app/components/home/home.tsx
+++ app/components/home/home.tsx
@@ -1,3 +1,16 @@
-function AllPosts() {}
+import { getAllPosts } from '@/app/services/apis'
+
+interface Posts {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
+
+async function AllPosts() {
+    const res = await getAllPosts()
+    const post: Posts[] = res.data
+    return <h1>{post[0].id}</h1>
+}
 
 export default AllPosts
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import AllPosts from "./components/home/home";
+
 export default function Home() {
-    return <main>saeed</main>
+    return <main>
+      <AllPosts />
+    </main>
 }
