f8f1c1ebb9555ff50e4e48c8a7eb5ecbfd538fc8
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-23 00:26:42 +0330

    Add react query to project and set it up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import ReduxStoreProvider from './provider/reduxStoreProvider'
+import ReactQueryProvider from './provider/ReactQueryProvider'
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -9,10 +10,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        // <ReduxStoreProvider>
-            <html>
-                <body>{children}</body>
-            </html>
-        // </ReduxStoreProvider>
+        <html>
+            <body>
+                <ReduxStoreProvider>
+                    <ReactQueryProvider>
+                        <>{children}</>
+                    </ReactQueryProvider>
+                </ReduxStoreProvider>
+            </body>
+        </html>
     )
 }
