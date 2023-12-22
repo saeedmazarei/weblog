52ac8b810c490f6abcb5d04661c3e37ee49cc7a9
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-22 16:23:37 +0330

    Refactor PostDetail component for showing data

--- app/components/posts/PostDetail.tsx
+++ app/components/posts/PostDetail.tsx
@@ -1,10 +1,15 @@
-import { getPostDetail } from "@/app/services/apis"
-import { Posts } from "@/app/types/types"
+import { getPostDetail } from '@/app/services/apis'
+import { Posts } from '@/app/types/types'
 
 async function PostDetail({ id }: { id: number }) {
     const res = await getPostDetail(id)
     const detail: Posts = res.data
-    return <h1>{detail.body}</h1>
+    return (
+        <>
+            <h1>{detail.title}</h1>
+            <p>{detail.body}</p>
+        </>
+    )
 }
 
 export default PostDetail
