6a15a0dcb48656a759d67f5266f8efd768d467ad
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-22 16:31:14 +0330

    Refactor PostItem component to show just 50 charachter of post body in home page

--- app/components/home/postItem.tsx
+++ app/components/home/postItem.tsx
@@ -1,10 +1,11 @@
 import { PostDetailProps } from '@/app/types/types'
 
 function PostItem({ post }: PostDetailProps) {
+    const truncatedBody = post.body.length > 50 ? `${post.body.substring(0, 50)}...` : post.body;
     return (
         <div>
             <h1>{post.title}</h1>
-            <p>{post.body}</p>
+            <p>{truncatedBody}</p>
         </div>
     )
 }
