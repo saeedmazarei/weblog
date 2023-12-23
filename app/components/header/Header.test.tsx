dfc946206979a45b39695bf4b4646e461fc850e5
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-23 09:14:39 +0330

    Add unit test for header component

---
+++ app/components/header/Header.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react';
+import Header from './header';
+
+describe('Header Component', () => {
+  it('renders with default props', () => {
+    const { getByText } = render(<Header />);
+    const linkElement = getByText(/overreacted/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders with custom style', () => {
+    const customStyle = { color: 'red' };
+    const { getByText } = render(<Header style={customStyle} />);
+    const linkElement = getByText(/overreacted/i);
+    expect(linkElement).toHaveStyle(customStyle);
+  });
+
+  // Add more test cases based on your component's behavior
+});
